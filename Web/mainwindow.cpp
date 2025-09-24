03d67df38f653d24c3ff6546d8a3e366ce05996d
Nikita Kroshko <nikita.kroshko@student.manchester.ac.uk>
2025-09-24 22:58:42 +0100

    make the right changes to mainwindow

--- Web/mainwindow.cpp
+++ Web/mainwindow.cpp
@@ -1,19 +1,59 @@
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
-#include <QWebEngineView>
+#include <QToolBar>
+#include <QAction>
+#include <QUrl>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
 
-    QWebEngineView *view = new QWebEngineView(this);
+    view = new QWebEngineView(this);
     view->setUrl(QUrl("https://www.google.com"));
     setCentralWidget(view);
+
+
+    QToolBar *toolbar = addToolBar("Navigation");
+
+
+    QAction *backAction = toolbar->addAction("⟵");
+    connect(backAction, &QAction::triggered, view, &QWebEngineView::back);
+
+
+    QAction *forwardAction = toolbar->addAction("⟶");
+    connect(forwardAction, &QAction::triggered, view, &QWebEngineView::forward);
+
+
+    QAction *reloadAction = toolbar->addAction("⟳");
+    connect(reloadAction, &QAction::triggered, view, &QWebEngineView::reload);
+
+
+    urlBar = new QLineEdit(this);
+    urlBar->setPlaceholderText("Enter URL and press Enter...");
+    toolbar->addWidget(urlBar);
+
+    connect(urlBar, &QLineEdit::returnPressed, this, &MainWindow::navigateToUrl);
+
+
+    connect(view, &QWebEngineView::urlChanged, this, &MainWindow::updateUrlBar);
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+}
+
+void MainWindow::navigateToUrl()
+{
+    QString urlText = urlBar->text();
+
+
+    if (!urlText.startsWith("http://") && !urlText.startsWith("https://")) {
+        urlText.prepend("https://");
+    }
+
+    view->setUrl(QUrl(urlText));
+}
+
+void MainWindow::updateUrlBar(const QUrl &url)
+{
+    urlBar->setText(url.toString());
 }
