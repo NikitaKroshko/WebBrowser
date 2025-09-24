476cfde7a05e3715dc2034466d39f2303ffe16e0
Nikita Kroshko <nikita.kroshko@student.manchester.ac.uk>
2025-09-24 22:55:02 +0100

    make mainwindow show google.com

--- Web/mainwindow.cpp
+++ Web/mainwindow.cpp
@@ -1,11 +1,16 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QWebEngineView>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QWebEngineView *view = new QWebEngineView(this);
+    view->setUrl(QUrl("https://www.google.com"));
+    setCentralWidget(view);
 }
 
 MainWindow::~MainWindow()
